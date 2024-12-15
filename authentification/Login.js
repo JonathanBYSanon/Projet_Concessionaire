99e538d880d66c131ec6eb1f1ffe880a573317e6
jonathan sanon <jonathansanon1999@gmail.com>
2024-12-15 00:07:09 -0500

    Ajout du role dans les information retourner par le login

--- authentification/Login.js
+++ authentification/Login.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { Utilisateur} from '../models/Relations.js';
+import { Utilisateur, Role} from '../models/Relations.js';
 import dotenv from 'dotenv';
 
 const ENV = dotenv.config().parsed;
@@ -8,7 +8,13 @@ const ENV = dotenv.config().parsed;
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await Utilisateur.findOne({ where: { email } });
+        const user = await Utilisateur.findOne({
+            where: { email },
+            include: {
+              model: Role,
+              attributes: ['titre'], // Récupère uniquement le titre du rôle
+            },
+          });
         if (!user) {
             return res.status(400).json({ message: 'Email ou mot de passe incorrect' });
         }
@@ -17,7 +23,7 @@ const login = async (req, res) => {
             return res.status(400).json({ message: 'Email ou mot de passe incorrect' });
         }
         const token = jwt.sign({ id: user.id }, ENV.JWT_SECRET, { expiresIn: '24h' });
-        res.status(200).json({ token:token, message:`Bienvenue ${user.prenom} ${user.nom}` });
+        res.status(200).json({ token:token, role:user.Role.titre, message:`Bienvenue ${user.prenom} ${user.nom}` });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
